11308a3880a093ba8d985ec315ea3ba191142a90
brkdq16 <brkdq16@gmail.com>
2026-05-27 19:50:24 -0400

    feat: add json persistence layer

--- Data/JsonStorage.cs
+++ Data/JsonStorage.cs
@@ -1,10 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text.Json;
 
 namespace SistemaAsistenciaEscolar.Data
 {
-    internal class JsonStorage
+    public class JsonStorage
     {
+        // GUARDAR DATOS
+
+        public void Guardar<T>(string rutaArchivo, List<T> datos)
+        {
+            // CONVERTIR OBJETOS A JSON
+
+            string json = JsonSerializer.Serialize(
+                datos,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+            // ESCRIBIR ARCHIVO
+
+            File.WriteAllText(rutaArchivo, json);
+        }
+
+        // CARGAR DATOS
+
+        public List<T> Cargar<T>(string rutaArchivo)
+        {
+            // SI EL ARCHIVO NO EXISTE
+
+            if (!File.Exists(rutaArchivo))
+            {
+                return new List<T>();
+            }
+
+            // LEER JSON
+
+            string json = File.ReadAllText(rutaArchivo);
+
+            // CONVERTIR JSON A OBJETOS
+
+            List<T> datos = JsonSerializer.Deserialize<List<T>>(json);
+
+            // SI VIENE NULL
+
+            if (datos == null)
+            {
+                return new List<T>();
+            }
+
+            return datos;
+        }
     }
 }
